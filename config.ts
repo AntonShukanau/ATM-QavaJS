92199ef7e595b0667f21b4ab705b4ead3be043df
Anton Shukanau <anton_shukanau@epam.com>
2023-10-15 21:51:04 +0300

    deps checks

--- config.ts
+++ config.ts
@@ -3,32 +3,23 @@ import App from "./page_object";
 
 export default {
   defaultTimeout: 60000,
-  parallel: 4,
-  paths: [
-    "features/**/*.feature"
-  ],
-  require: [
-    "step_definition/*.ts",
-    "node_modules/@qavajs/steps-playwright/index.js",
-    "node_modules/@qavajs/steps-memory/index.js"
-  ],
-  requireModule: [
-    "@qavajs/template"
-  ],
-  format: [
-    "@qavajs/console-formatter",
-    "@qavajs/html-formatter:report/report.html"
-  ],
+  parallel: 8,
+  paths: ["features/**/*.feature"],
+  require: ["step_definition/*.ts","node_modules/@qavajs/steps-playwright/index.js","node_modules/@qavajs/steps-memory/index.js"],
+  requireModule: ["@qavajs/template"],
+  format: ["@qavajs/console-formatter","@qavajs/html-formatter:report/report.html"],
   memory: new Memory(),
   pageObject: new App(),
   browser: {
     capabilities: {
       browserName: "chromium",
+      channel: 'chrome',
       headless: true,
       viewport: { height: 720, width: 1366 }
+    },
+    timeout: {
+      page: 20000
     }
   },
-  templates: [
-    "templates/*.feature"
-  ],
+  templates: ["templates/*.feature"]
 }
