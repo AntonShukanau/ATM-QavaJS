135654b18cd8f069821bb96a0845345e5c0bb0d8
Anton Shukanau <anton_shukanau@epam.com>
2023-10-22 11:13:15 +0300

    deleted additional method

--- support/utils.ts
+++ support/utils.ts
@@ -3,9 +3,3 @@ import { Response } from "playwright";
 export function isSuccessful(url: string, response: Response) {
     return response.url().includes(url) && response.status() === 200 || 201 || 202;
 }
-
-export function titleSpaceReplacer(str: string) {
-  let parts = str.split("#");
-  parts[0] = parts[0] + " ";
-  str = parts.join("#");
-}
