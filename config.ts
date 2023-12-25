ba98ade3cf344f936721300ba8d1778afaf364a4
Anton Shukanau <anton_shukanau@epam.com>
2023-12-25 10:08:53 +0300

    test

--- config.ts
+++ config.ts
@@ -18,7 +18,7 @@ export default {
       description: 'Description',
       tags: ['Tag'],
       project: 'anton_shukanau_personal',
-      launch: 'Test',
+      launch: 'Test ATM',
       mode: 'DEFAULT',
       retry: 1,
       ignoreErrors: false, 
